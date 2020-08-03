6268fcf79f825562eb481b128a9be5b2be97c89f
girinssh <tngus5851@naver.com>
2020-08-03 14:01:10 +0900

    first commit

---
+++ BOJ3895.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
+
+	while (true) {
+		int n, k, m;
+
+		cin >> n >> k >> m;
+
+		list<int> l;
+		list<int>::iterator iter;
+
+
+
+	}
+}
